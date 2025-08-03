cfb808ec559aae517cd7e45812f6bd09efe68368
vickyamber <k.laohasiri@gmail.com>
2025-08-03 19:12:41 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 self.addEventListener('install', (event) => {
   console.log('Service worker installing...');
+  // Add a call to skipWaiting here
   self.skipWaiting();
 });
 
@@ -8,5 +9,7 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
+  // We are not adding caching logic here to keep it simple,
+  // but this file is required for PWA installability.
   return;
 });
