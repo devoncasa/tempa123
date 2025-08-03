94488f927295c39475f58e90552706136480fab4
vickyamber <k.laohasiri@gmail.com>
2025-08-03 19:09:10 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
+self.addEventListener('install', (event) => {
+  console.log('Service worker installing...');
+  self.skipWaiting();
+});
 
+self.addEventListener('activate', (event) => {
+  console.log('Service worker activating...');
+});
+
+self.addEventListener('fetch', (event) => {
+  return;
+});
